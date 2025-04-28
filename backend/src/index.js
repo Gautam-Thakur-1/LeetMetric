4f929494a030599f913eb39f43a7a24bd6b7f6d8
Gautam-Thakur-1 <thakurgautam1079@gmail.com>
2025-04-28 18:43:46 +0530

    big fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,41 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import leetcodeRoutes from "./routes/leetcode.js";
+  import express from "express";
+  import dotenv from "dotenv";
+  import cors from "cors";
+  import leetcodeRoutes from "./routes/leetcode.js";
 
-dotenv.config();
+  dotenv.config();
 
-const app = express();
-const PORT = 3000;
+  const app = express();
+  const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://leetmetrix.netlify.app/",
-];
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://leetmetrix.netlify.app",
+  ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"), false);
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"), false);
+        }
+      },
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
-app.use("/api", leetcodeRoutes);
+  app.use("/api", leetcodeRoutes);
 
-app.get("/", (req, res) => {
-  res.send({
-    Routes: ["/profile/:username", "/profile/:username/streak"],
+  app.get("/", (req, res) => {
+    res.send({
+      Routes: ["/profile/:username", "/profile/:username/streak"],
+    });
   });
-});
 
-export default app;
+  export default app;
